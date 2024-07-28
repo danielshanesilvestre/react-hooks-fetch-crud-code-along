3652474914c7c1a5544a2114dadb88b73019915b
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-28 10:57:29 -0500

    Completed code-along

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -17,6 +17,22 @@ function ShoppingList() {
     setItems([...items, newItem]);
   }
 
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
@@ -36,7 +52,12 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item
+            key={item.id}
+            item={item}
+            onUpdateItem={handleUpdateItem} 
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
